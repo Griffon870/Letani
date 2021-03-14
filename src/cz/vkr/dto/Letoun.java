e0f69ad69a304b7eec6148d8731279ad23da2ed6
Vojtech Kral <vkral@trask.cz>
2021-03-14 22:33:22 +0100

    kolekce

--- src/cz/vkr/dto/Letoun.java
+++ src/cz/vkr/dto/Letoun.java
@@ -12,8 +12,7 @@ public abstract class Letoun {
 	private int cenaLetoveHodiny;
 	private int vne;
 	private int cenaLetadla;
-	
-		
+
 	public Letoun(String registrace, String nazev, int rozpeti, int cenaLetoveHodiny, int vne, int cenaLetadla) {
 		super();
 		this.registrace = registrace;
@@ -23,73 +22,81 @@ public abstract class Letoun {
 		this.vne = vne;
 		this.cenaLetadla = cenaLetadla;
 	}
+
 	public String getRegistrace() {
 		return registrace;
 	}
+
 	public void setRegistrace(String registrace) {
 		this.registrace = registrace;
 	}
+
 	public String getNazev() {
 		return nazev;
 	}
+
 	public void setNazev(String nazev) {
 		this.nazev = nazev;
 	}
+
 	public int getRozpeti() {
 		return rozpeti;
 	}
+
 	public void setRozpeti(int rozpeti) {
 		this.rozpeti = rozpeti;
 	}
+
 	public int getCenaLetoveHodiny() {
 		return cenaLetoveHodiny;
 	}
+
 	public void setCenaLetoveHodiny(int cenaLetoveHodiny) {
 		this.cenaLetoveHodiny = cenaLetoveHodiny;
 	}
+
 	public int getVne() {
 		return vne;
 	}
+
 	public void setVne(int vne) {
 		this.vne = vne;
 	}
+
 	public int getCenaLetadla() {
 		return cenaLetadla;
 	}
+
 	public void setCenaLetadla(int cenaLetadla) {
 		this.cenaLetadla = cenaLetadla;
 	}
-	
-	
+
 	public String getFormatovanaCenaLetadla() {
-		
-		return getCenaLetadla()+Konstatnty.CESKA_KORUNA;
-		
+
+		return getCenaLetadla() + Konstatnty.CESKA_KORUNA;
+
 	}
-	
-	
+
 	public static Comparator<Letoun> LetounNazevComparator = new Comparator<Letoun>() {
 
 		public int compare(Letoun l1, Letoun l2) {
-		   String letounNazev1 = l1.getNazev().toUpperCase();
-		   String letounNazev2 = l2.getNazev().toUpperCase();
+			String letounNazev1 = l1.getNazev().toUpperCase();
+			String letounNazev2 = l2.getNazev().toUpperCase();
 
-		   return letounNazev1.compareTo(letounNazev2);		  
-	    }};
+			return letounNazev1.compareTo(letounNazev2);
+		}
+	};
 
-	    
-	    public static Comparator<Letoun> LetounCenaComparator = new Comparator<Letoun>() {
+	public static Comparator<Letoun> LetounCenaComparator = new Comparator<Letoun>() {
 
 		public int compare(Letoun l1, Letoun l2) {
 
-		   int cena1 = l1.getCenaLetadla();
-		   int cena2 = l2.getCenaLetadla();
+			int cena1 = l1.getCenaLetadla();
+			int cena2 = l2.getCenaLetadla();
 
-		   return cena1-cena2;
+			return cena1 - cena2;
 
-	   }};
-	
-	
-	
-	
+		}
+	};
+
 }
